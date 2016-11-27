5d2aa4c607780b7cf3ac4a86cee8b2f5ae6b9be6
sigalor <juna99@gmx.de>
2016-11-27 21:38:32 +0100

    Backup commit

--- dep/StatementList.d
+++
@@ -1,8 +0,0 @@
-obj/StatementList.o: src/StatementList.cpp include/StatementList.hpp \
- include/Node.hpp include/Statement.hpp
-
-include/StatementList.hpp:
-
-include/Node.hpp:
-
-include/Statement.hpp:
